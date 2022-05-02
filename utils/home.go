1c9a7fde8a6e72d428f2f1c19585537dd92e5a56
Arthur Kalikiti <arthur.kalikiti@infratel.co.zm>
2022-05-02 20:29:49 +0200

    fix: Fixed application inability to run a vm after definition

--- utils/home.go
+++ utils/home.go
@@ -8,13 +8,14 @@ import (
 // SwiftHome returns apps configs directory
 // Creates one if it does not exist
 func SwiftHome() (string, error) {
-	dir, err := os.UserConfigDir()
-	if err != nil {
-		return "", fmt.Errorf("%v", err)
-	}
-	path := dir + "/swift"
+	path := "/var/swift"
+
+	// Only create the directory if it doesn't exist
 	if _, err := os.Stat(path); err != nil {
-		if er := os.Mkdir(path, 0777); er != nil {
+		if er := os.Mkdir(path, 0775); er != nil {
+			return "", fmt.Errorf("%v", er)
+		}
+		if er := os.Chown(path, os.Getuid(), os.Getgid()); er != nil {
 			return "", fmt.Errorf("%v", er)
 		}
 	}
